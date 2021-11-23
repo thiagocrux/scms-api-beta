e87a97f9e9234eda24f39859e6652a18f582d697
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-11-23 14:05:27 -0300

    feat(index.ts): add database connection function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import connectDatabase from './utils/connectDatabase';
 import logger from './utils/logger';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3333, () =>
-  logger.info('Server listening at http://localhost:3333')
-);
+app.listen(3333, async () => {
+  logger.info('Server listening at http://localhost:3333');
+  await connectDatabase();
+});
