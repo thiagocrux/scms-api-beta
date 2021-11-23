49464baf942a13a01c0059207c04e00bdf898631
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-11-23 14:00:52 -0300

    refactor(logger.ts): update deprecated code

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,11 +2,16 @@ import logger from 'pino';
 import dayjs from 'dayjs';
 
 const log = logger({
-  prettyPrint: true,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: `${dayjs().format()}`,
+    },
+  },
   base: {
     pid: false,
   },
-  timestamp: () => `"time":"${dayjs().format()}"`,
 });
 
 export default log;
