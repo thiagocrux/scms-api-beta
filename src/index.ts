8a3fc3e6f6803c44068c9a4bd357a25652258993
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-11-26 00:15:12 -0300

    refactor(index.ts): update hardcoded port for value stored in environment variable

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import connectDatabase from './utils/connectDatabase';
 import logger from './utils/logger';
 
 const app = express();
+const port = process.env.API_PORT;
 
 app.use(express.json());
 
-app.listen(3333, async () => {
-  logger.info('Server listening at http://localhost:3333');
+app.listen(port, async () => {
+  logger.info(`Server listening at http://localhost:${port}`);
   connectDatabase();
   routes(app);
 });
