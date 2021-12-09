dac6c0d6d71e498dbb3a0e1b0279963b15022447
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-12-08 23:30:23 -0300

    feat(observation.model.ts): create a observation model

---
+++ src/models/observation.model.ts
@@ -0,0 +1,32 @@
+import { Document, model, Schema } from 'mongoose';
+import { PatientDocument } from './patient.model';
+
+export interface ObservationInput {
+  observations: string | null;
+  partnerBeingTreated: boolean;
+  patient: PatientDocument['_id'];
+}
+
+export interface ObservationDocument extends ObservationInput, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const observationSchema = new Schema<ObservationDocument>(
+  {
+    observations: { type: String, default: null },
+    partnerBeingTreated: { type: Boolean, default: false },
+    patient: {
+      type: Schema.Types.ObjectId,
+      ref: 'Patient',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ObservationModel = model<ObservationDocument>('Exam', observationSchema);
+
+export default ObservationModel;
